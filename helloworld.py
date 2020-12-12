bebdceb10c44231519e38930f6969c4cd37b70a1
Psych-Kicks <orhan_yil@msn.com>
2020-12-13 01:07:57 +0300

    winAPI Python Projects

---
+++ helloworld.py
@@ -0,0 +1,23 @@
+import ctypes
+
+
+user_handle = ctypes.WinDLL("user32.dll")
+k_handle = ctypes.WinDLL("kernel32.dll")
+
+hWnd = None
+lpText = "Hellow World!"
+lpCaption = "Hello Caption"
+uType = 0x00000001
+
+
+response = user_handle.MessageBoxW(hWnd,lpText,lpCaption,uType)
+
+error = k_handle.GetLastError()
+if error != 0:
+	print("Error Code: {0}".format(error))
+	exit(1)
+
+if response == 1:
+	print("User Clicked OK!")
+elif response == 2:
+	print("User Clicked Cancel!")
